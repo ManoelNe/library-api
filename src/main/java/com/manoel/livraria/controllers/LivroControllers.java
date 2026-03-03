91dc387ec3df680ce0462d9e7dda7440e7c79861
ManoelNe <manoel_neto@outlook.com>
2026-03-03 15:59:56 -0300

    feat: CRUD

--- src/main/java/com/manoel/livraria/controllers/LivroControllers.java
+++ src/main/java/com/manoel/livraria/controllers/LivroControllers.java
@@ -1,4 +1,40 @@
 package com.manoel.livraria.controllers;
 
+import com.manoel.livraria.dto.LivroDTO;
+import com.manoel.livraria.services.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/livros")
 public class LivroControllers {
+
+    @Autowired
+    private LivroService service;
+
+    @PostMapping
+    public LivroDTO insert(@RequestBody LivroDTO dto){
+        dto = service.insert(dto);
+        return dto;
+    }
+
+    @GetMapping("/{id}")
+    public LivroDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+
+    @GetMapping
+    public Page<LivroDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
+    }
 }
--- src/main/java/com/manoel/livraria/services/LivroService.java
+++ src/main/java/com/manoel/livraria/services/LivroService.java
@@ -4,10 +4,15 @@ import com.manoel.livraria.dto.LivroDTO;
 import com.manoel.livraria.entities.Livro;
 import com.manoel.livraria.repositories.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.List;
+
+import static org.hibernate.internal.util.collections.CollectionHelper.map;
 
 @Service
 public class LivroService {
@@ -22,6 +27,21 @@ public class LivroService {
         livro = repository.save(livro);
         return new LivroDTO(livro);
 
+    }
+
+    @Transactional(readOnly = true)
+    public LivroDTO findById(Long id) {
+
+        Livro entity = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Livro não encontrado"));
+
+        return new LivroDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<LivroDTO> findAll(Pageable pageable) {
+         Page<Livro> result = repository.findAll(pageable);
+         return result.map(x-> new LivroDTO(x));
     }
 
     private void copyDtoToEntity(LivroDTO dto, Livro entity) {
