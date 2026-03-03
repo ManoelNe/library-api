1da4876e06e9810409bd4ba1f9fe5004183d1092
ManoelNe <manoel_neto@outlook.com>
2026-03-03 15:15:50 -0300

    feat: implement insert method in Livro service

--- src/main/java/com/manoel/livraria/dto/LivroDTO.java
+++ src/main/java/com/manoel/livraria/dto/LivroDTO.java
@@ -1,42 +1,53 @@
-package com.manoel.livraria.dto;
+    package com.manoel.livraria.dto;
 
-import java.time.Instant;
+    import com.manoel.livraria.entities.Livro;
 
-public class LivroDTO {
+    import java.time.Instant;
 
-    private Long id;
-    private String autor;
-    private String titulo;
-    private Instant dataCadastro;
-    private Integer anoPublicacao;
+    public class LivroDTO {
 
-    public LivroDTO(Long id, String autor, String titulo, Instant dataCadastro, Integer anoPublicacao) {
-        this.id = id;
-        this.autor = autor;
-        this.titulo = titulo;
-        this.dataCadastro = dataCadastro;
-        this.anoPublicacao = anoPublicacao;
+        private Long id;
+        private String autor;
+        private String titulo;
+        private Instant dataCadastro;
+        private Integer anoPublicacao;
+
+        public LivroDTO(Long id, String autor, String titulo, Instant dataCadastro, Integer anoPublicacao) {
+            this.id = id;
+            this.autor = autor;
+            this.titulo = titulo;
+            this.dataCadastro = dataCadastro;
+            this.anoPublicacao = anoPublicacao;
+        }
+        public LivroDTO(Livro entity) {
+            id = entity.getId();
+            autor = entity.getAutor();
+            titulo = entity.getTitulo();
+            dataCadastro = entity.getDataCadastro();
+            anoPublicacao = entity.getAnoPublicacao();
+        }
+
+
+        public LivroDTO(){}
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getAutor() {
+            return autor;
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public Instant getDataCadastro() {
+            return dataCadastro;
+        }
+
+        public Integer getAnoPublicacao() {
+            return anoPublicacao;
+        }
+
     }
-
-    public LivroDTO(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public Instant getDataCadastro() {
-        return dataCadastro;
-    }
-
-    public Integer getAnoPublicacao() {
-        return anoPublicacao;
-    }
-}
--- src/main/java/com/manoel/livraria/services/LivroService.java
+++ src/main/java/com/manoel/livraria/services/LivroService.java
@@ -1,4 +1,34 @@
 package com.manoel.livraria.services;
 
+import com.manoel.livraria.dto.LivroDTO;
+import com.manoel.livraria.entities.Livro;
+import com.manoel.livraria.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+@Service
 public class LivroService {
+
+    @Autowired
+    private LivroRepository repository;
+
+    @Transactional
+    public LivroDTO insert(LivroDTO dto){
+        Livro livro = new Livro();
+        copyDtoToEntity(dto, livro);
+        livro = repository.save(livro);
+        return new LivroDTO(livro);
+
+    }
+
+    private void copyDtoToEntity(LivroDTO dto, Livro entity) {
+        entity.setAutor(dto.getAutor());
+        entity.setTitulo(dto.getTitulo());
+        entity.setAnoPublicacao(dto.getAnoPublicacao());
+        entity.setDataCadastro(Instant.now());
+    }
+
 }
